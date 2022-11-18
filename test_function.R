4654252366b6e2b29e8ec34365e678b771463a30
rentianai <114671172+rentianai@users.noreply.github.com>
2022-11-18 10:48:02 +0000

    Update test_function.R
    slightly adjustive test

--- test_function.R
+++ test_function.R
@@ -21,4 +21,19 @@ hb <- function(th,k=2) {
 }
 
 
-newt(c(-29,100),rb,gb,hb)
+newt(c(-25,100),rb,gb,hb)
+hb(c(-25,100),k=2)
+
+# test if fx, gx is not finite
+newt(c(-25,-Inf),rb,gb,hb)
+
+# test when H is not positive definiteness, because not positive, Hi=null, but still calculate
+# test if iterations fail to converge
+h1 <- function(th,k=2) {
+  h <- matrix(0,2,2)
+  h[1,1] <- h[2,2] <- th[1]
+  h[1,2] <- h[2,1] <- th[2]
+  h
+  #-x^(-2)
+}
+newt(c(-25,100),rb,gb,h1)
